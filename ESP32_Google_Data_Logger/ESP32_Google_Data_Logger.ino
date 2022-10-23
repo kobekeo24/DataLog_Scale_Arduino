cda41233bf3b4e07ff404828444fcfad7981e3f6
kobekeo24 <kobekeo@gmail.com>
2022-10-23 18:35:46 -0400

    Added code from HX711 example to display weight to LCD. Need to to calibrate.

--- ESP32_Google_Data_Logger/ESP32_Google_Data_Logger.ino
+++ ESP32_Google_Data_Logger/ESP32_Google_Data_Logger.ino
@@ -1,4 +1,7 @@
 #include <HX711_ADC.h>
+#if defined(ESP8266)|| defined(ESP32) || defined(AVR)
+#include <EEPROM.h>
+#endif
 
 #include <LiquidCrystal_I2C.h>
 
@@ -14,6 +17,10 @@
 #define RXD2 16
 #define TXD2 17
 
+//pins:
+const int HX711_dout = 4; //mcu > HX711 dout pin
+const int HX711_sck = 5; //mcu > HX711 sck pin
+
 int analogPinX = 35;
 int analogPinY = 34; 
 int X_value,Y_Value;
@@ -28,11 +35,18 @@ const char * password = "Zoinks1398.";
 String GOOGLE_SCRIPT_ID = "AKfycbwgr-4Nd6BF9EO2Wr00D8dKJQtetk5sjSqPGa2m8flkQyBd6rxRAcq_T1b8qGtZvvwH";
 //---------------------------------------------------------------------
 
+//HX711 constructor:
+HX711_ADC LoadCell(HX711_dout, HX711_sck);
+
+const int calVal_eepromAdress = 0;
+unsigned long t = 0;
+//---------------------------------------------------------------------
 const int sendInterval = 2000;
 String param;
 int nDevices = 0;
 String X_string = "X_coord=";
 String Y_string = "Y_coord=";
+String Weight_s = "Weight=";
 // Set the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 /************************************************************************************
@@ -43,7 +57,29 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
     lcd.begin();
     lcd.backlight();
   //--------------------------------------------
-  Serial.begin(115200);
+  Serial.begin(57600);
+
+  LoadCell.begin();
+  //LoadCell.setReverseOutput(); //uncomment to turn a negative output value to positive
+  float calibrationValue; // calibration value (see example file "Calibration.ino")
+  calibrationValue = 696.0; // uncomment this if you want to set the calibration value in the sketch
+#if defined(ESP8266)|| defined(ESP32)
+  //EEPROM.begin(512); // uncomment this if you use ESP8266/ESP32 and want to fetch the calibration value from eeprom
+#endif
+  //EEPROM.get(calVal_eepromAdress, calibrationValue); // uncomment this if you want to fetch the calibration value from eeprom
+
+  unsigned long stabilizingtime = 2000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
+  boolean _tare = true; //set this to false if you don't want tare to be performed in the next step
+  LoadCell.start(stabilizingtime, _tare);
+  if (LoadCell.getTareTimeoutFlag()) {
+    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
+    while (1);
+  }
+  else {
+    LoadCell.setCalFactor(calibrationValue); // set calibration value (float)
+    Serial.println("Startup is complete");
+  }
+  
   //start serial communication with Serial Monitor
   //--------------------------------------------
   //start serial communication with Neo6mGPS
@@ -73,27 +109,54 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
  *  loop function starts
  **********************************************************************************/
 void loop() {
-  X_value = analogRead(analogPinX);
-  Y_Value = analogRead(analogPinY);
-  param  = "X_coord="+String(X_value);
-  param += "&Y_coord="+String(Y_Value);
+    static boolean newDataReady = 0;
+  const int serialPrintInterval = 0; //increase value to slow down serial print activity
+
+  // check for new data/start next conversion:
+  if (LoadCell.update()) newDataReady = true;
+
+  // get smoothed value from the dataset:
+  if (newDataReady) {
+    if (millis() > t + serialPrintInterval) {
+      float i = LoadCell.getData();
+      Serial.print("Load_cell output val: ");
+      Serial.println(i);
+      newDataReady = 0;
+      t = millis();
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print(Weight_s);
+      
+      lcd.setCursor(Weight_s.length()+2,0);
+      lcd.print(i);
+    }
+  }
+
+  // receive command from serial terminal, send 't' to initiate tare operation:
+  if (Serial.available() > 0) {
+    char inByte = Serial.read();
+    if (inByte == 't') LoadCell.tareNoDelay();
+  }
+
+  // check if last tare operation is complete:
+  if (LoadCell.getTareStatus() == true) {
+    Serial.println("Tare complete");
+  }
+
+
   
-  Serial.println(param);
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(X_string);
-  lcd.setCursor(0,1);
-  lcd.print(Y_string);
+  //X_value = analogRead(analogPinX);
+  //Y_Value = analogRead(analogPinY);
+  //param  = "X_coord="+String(X_value);
+  //param += "&Y_coord="+String(Y_Value);
   
-  lcd.setCursor(X_string.length()+2,0);
-  lcd.print(X_value);
-  lcd.setCursor(Y_string.length()+2,1);
-  lcd.print(Y_Value);
+  //Serial.println(param);
+
 
   //write_to_google_sheet(param);
 
   
-  delay(sendInterval);
+  //delay(sendInterval);
 }
 //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
 
